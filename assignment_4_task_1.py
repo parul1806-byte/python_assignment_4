f7917a7e47159a5723bdd93e0bdb72cf01e7b822
parul pal <parulpal7088@gmail.com>
2025-07-08 17:45:13 +0530

    Add files via upload

---
+++ assignment_4_task_1.py
@@ -0,0 +1,18 @@
+# Opens and reads a text file named sample.txt
+##Prints its content line by line.
+
+try:
+    f = open('sample.txt','r')
+    content = f.readlines()
+    for line in content:
+        print(line.strip())
+
+except FileNotFoundError:
+    print(" error :The file sample.txt not found")
+
+
+
+
+
+
+
---
+++ assignment_4_task_2.py
@@ -0,0 +1,29 @@
+#1.   Takes user input and writes it to a file named output.txt.
+from logging import exception
+
+try:
+    text = input("enter the text write to the file:")
+    f = open('output.txt', 'w')
+    f.write(text + "\n")
+    print("data sucessfully written to output.txt\n")
+except exception as e:
+    print(" error while writing the file",e)
+f.close()
+
+#2.Appends additional data to the same file.
+try:
+    add = input("enter additional data:")
+    f = open('output.txt', 'a')
+    f.write(add + "\n")
+    print("data sucessfully appended\n")
+except exception as e:
+     print("error while appending the file",e)
+f.close()
+
+#3.   Reads and displays the final content of the file.
+print("final content of output.txt")
+f = open('output.txt','r')
+content = f.readlines()
+for line in content:
+    print(line.strip())
+f.close()
